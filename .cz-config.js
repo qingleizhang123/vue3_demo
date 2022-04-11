3da1167d78920f631ff9c20c14efdd2acd450259
qinglei.zhang <qinglei.zhang@united-imaging.com>
2022-04-11 09:24:55 +0800

    fix: bug修复

--- .cz-config.js
+++ .cz-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat: 一个新的功能' },
-    { value: 'fix', name: 'fix: 修复一个Bug' },
-    { value: 'docs', name: 'docs: 变更的只有文档' },
-    { value: 'style', name: 'style: 代码风格,格式修复' },
-    { value: 'refactor', name: 'refactor: 代码重构，注意和feat、fix区分开' },
-    { value: 'perf', name: 'perf: 代码优化,改善性能' },
-    { value: 'test', name: 'test: 测试' },
-    { alue: 'chore', name: 'chore: 变更构建流程或辅助工具' },
-    { value: 'revert', name: 'revert: 代码回退' },
+    { value: '✨feat', name: 'feat: 一个新的功能' },
+    { value: '🐛fix', name: 'fix: 修复一个Bug' },
+    { value: '📝docs', name: 'docs: 变更的只有文档' },
+    { value: '💄style', name: 'style: 代码风格,格式修复' },
+    { value: '♻️refactor', name: 'refactor: 代码重构,注意和feat、fix区分开' },
+    { value: '⚡️perf', name: 'perf: 代码优化,改善性能' },
+    { value: '✅test', name: 'test: 测试' },
+    { alue: '🔧chore', name: 'chore: 变更构建流程或辅助工具' },
+    { value: '⏪revert', name: 'revert: 代码回退' },
     { value: 'init', name: 'init: 项目初始化' },
     { value: 'build', name: 'build: 变更项目构建或外部依赖' },
     { value: 'WIP', name: 'WIP: 进行中的工作' }
@@ -36,18 +36,15 @@ module.exports = {
     // used if allowCustomScopes is true
     customScope: 'Denote the SCOPE of this change:',
     subject: '简短说明(最多40个字):',
-    body: '长说明，使用"|"换行(可选)：\n',
+    body: '长说明，使用"|"换行(可选):\n',
     breaking: '非兼容性说明 (可选):\n',
-    footer: '关联关闭的issue，例如：#12, #34(可选):\n',
+    footer: '关联关闭的issue,例如：#12, #34(可选):\n',
     confirmCommit: '确定提交?'
   },
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   // skip any questions you want
-  skipQuestions: ['scope', 'breaking'],
+  skipQuestions: ['scope', 'footer', 'breaking'],
   // limit subject length
   subjectLimit: 100
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true, // default is false
 }
